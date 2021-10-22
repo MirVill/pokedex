c6f05f481d166057360be2404d57371476f72381
John Doe <miriamvillazaragoza@gmail.com>
2021-10-21 22:29:14 -0500

    import router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-
-import PokemonsList from "./components/Pokemons/PokemonsList";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { useState } from "react";
+import PokemonsList from "./pages/PokemonsList";
 import Header from "./components/ui/Header";
+import PokemonInfo from "./pages/PokemonInfo";
 
 
 function App() {
+  const [pokemonDetailsUrl, setPokemonDetailsUrl] = useState('');
+
+  const getPokemonDetailsUrl = (url) => {
+    setPokemonDetailsUrl(url)
+  };
+
+
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <PokemonsList />
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/pokemons" />
+          </Route>
+          <Route path="/pokemons" exact>
+            <PokemonsList onGetPokemonData={getPokemonDetailsUrl}/>
+          </Route>
+          <Route path="/pokemons/:pokemonId">
+            <PokemonInfo url = {pokemonDetailsUrl}/>
+          </Route>
+        </Switch>
       </main>
     </div>
   );
